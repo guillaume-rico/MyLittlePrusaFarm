a1506995d26774845d7a1e304f20068532378934
Alliaume <alliaume.rico@gmail.com>
2023-12-15 09:30:32 +0100

    MAJ

--- MyLittlePrusaFarm.py
+++ MyLittlePrusaFarm.py
@@ -1,32 +1,55 @@
 #!/usr/bin/python
 # python C:\SLF\Perso\MyLittlePrusaFarm\MyLittlePrusaFarm.py
-import configparser
+
+import argparse
 import os
 import glob 
+import sys 
+
+sys.path.insert(1, 'C:/SLF/Perso/brio/pyPrusaLink/PrusaLinkPy')
+
 import PrusaLinkPy
 
 # On récupère le nom du dossier du script
 scriptPath = os.path.dirname(os.path.realpath(__file__))
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--clean', action='store_true')
+parser.add_argument('-t', '--test', action='store_true')
+parser.add_argument('-g', '--groups', nargs='+', default=[])
+args = parser.parse_args()
+clean = args.clean
+test = args.test
+groupListToDo = args.groups
+
+def flatten(dictionary, folder ="") :
+    items = []
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            for elem in flatten(value,folder + key + "/") :
+                items.append(elem)
+        else:
+            items.append((folder + key,value))
+    return items
+
+def flattenFolder(dictionary, folder ="") :
+    items = []
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            items.append(folder + key)
+            for elem in flattenFolder(value,folder + key + "/") :
+                items.append(elem)
+    return items
+
 # On vérifie si un dossier _COMMON est présent 
 dictGcodeCommon = {}
 commonPath = scriptPath + "/groups"  + "/_COMMON"
-if os.path.exists(commonPath) :
-    print("Folder with common GCODE exists")
-    listeTemp = os.scandir(commonPath)
-    for folder in listeTemp :
-        if folder.is_dir() :
-            dictGcodeCommon[folder.name] = {}
-            for file in os.scandir(folder) :
-                if "gcode" in file.name :
-                    dictGcodeCommon[folder.name][file.name] = file.path
-        elif "gcode" in folder.name :
-            dictGcodeCommon[folder.name] = folder.path
-    print(dictGcodeCommon)
-    
-    
+
 for folderGroup in os.scandir(scriptPath + '/groups') :
-    if folderGroup.is_dir() and folderGroup.name != "_COMMON" and folderGroup.name != "example" :
+    if folderGroup.is_dir() and \
+       folderGroup.name != "_COMMON" and \
+       folderGroup.name != "example" and \
+       (groupListToDo == [] or folderGroup.name in groupListToDo) :
         print("Working on group : " + folderGroup.name)
         # Pour chaque machine :
         # 1) On se connecte 
@@ -67,53 +90,71 @@ for folderGroup in os.scandir(scriptPath + '/groups') :
                         connectionOK = False
                         print("<!>  Error - Host does not respond")
                 
+                    listFileToSend = []
+                    listFolderToSend = []
+                    
                     if connectionOK :
-                        # On vient charger tous les COMMON
-                        if len(dictGcodeCommon) != 0 :
-                            for folder in dictGcodeCommon :
-                                if isinstance(dictGcodeCommon[folder], dict) :
-                                    print("  - Folder : " + folder)
-                                    tempFolder = folder
-                                    for file in dictGcodeCommon[folder] :
-                                        print("   - File : " + file, end="")
-                                        if prusaMini.exists_gcode(tempFolder + "/" + file) : 
-                                            print(" -> File Already Exists")
-                                        else :
-                                            print(" -> Sending file", end="")
-                                            ret = prusaMini.put_gcode(dictGcodeCommon[folder][file], tempFolder + "/" + file)
-                                            print(" - Status : " + str(ret.status_code) + " text "  + str(ret.text))
-                                else :
-                                    print("  - File : " + folder, end="")
-                                    if prusaMini.exists_gcode(folder) : 
-                                        print(" -> File Already Exists")
-                                    else :
-                                        print(" -> Sending file", end="")
-                                        ret = prusaMini.put_gcode(dictGcodeCommon[folder], folder)
-                                        print(" - Status : " + str(ret.status_code) + " text "  + str(ret.text))
-                                    
-                                    
-                        # On synchronise ensuite les dossiers présents dans GCODE
-                        if os.path.exists(folderGroup.path + "/GCODE") :
-                            for folderGcode in os.scandir(folderGroup.path + '/GCODE/') :
-                                if folderGcode.is_dir() :
-                                    tempFolder = folderGcode.name
-                                    for file in os.scandir(folderGcode) :
-                                        if "gcode" in file.name :
-                                            print("   - File : " + file.name, end="")
-                                            if prusaMini.exists_gcode(tempFolder + "/" + file.name) : 
+                        # On vient télécharger tous les fichiers de COMMON et GCODE
+                        listDirToSynch = [commonPath, folderGroup.path + "/GCODE"]
+                        for dirToSynch in listDirToSynch :
+                            dirToSynch = dirToSynch.replace("\\","/")
+                            if os.path.exists(dirToSynch) :
+                                for (root,dirs,files) in os.walk(dirToSynch,topdown=True):
+                                    for file in files :
+                                        if "gcode" in file : 
+                                            completPath = root.replace("\\","/") + "/" + file
+                                            printerPath = completPath.replace(dirToSynch + "/","")
+                                            listFolderToSend.append(root.replace("\\","/").replace(dirToSynch + "/",""))
+                                            #print("Copy from : " + completPath + " to " + printerPath, end="")
+                                            print("  * File " + printerPath, end="")
+                                            listFileToSend.append(printerPath)
+                                            if prusaMini.exists_gcode(printerPath) : 
                                                 print(" -> File Already Exists")
                                             else :
                                                 print(" -> Sending file", end="")
-                                                ret = prusaMini.put_gcode(file.path, tempFolder + "/" + file.name)
-                                                print(" - Status : " + str(ret.status_code) + " text "  + str(ret.text))
-                                elif "gcode" in folderGcode.name :
-                                    print("   - File : " + folderGcode.name, end="")
-                                    if prusaMini.exists_gcode(folderGcode.name) : 
-                                        print(" -> File Already Exists")
+                                                errorDuringSending = False
+                                                try :
+                                                    if not test :
+                                                        ret = prusaMini.put_gcode(completPath, printerPath)
+                                                except :
+                                                    print(" -> Error durring sending")
+                                                    errorDuringSending = True
+                                                    break
+                                                if not errorDuringSending : 
+                                                    if ret.status_code == 201 :
+                                                        print(" -> Done")
+                                                    elif ret.status_code == 401 :
+                                                        print(" -> Error : Unauthorized - Check IP & Port & PKA") 
+                                                    elif ret.status_code == 409 :
+                                                        print(" -> Error : File already exists") 
+                                                    else :
+                                                        print(" -> Error : " + str(ret.status_code) + " text "  + str(ret.text))
+                        # On Supprime les fichiers qui n'ont plus rien a faire
+                        listFolderToSend = list(set(listFolderToSend))
+                        #print(listFolderToSend)
+                        if clean :
+                            # filename filedir
+                            fileDict = prusaMini.get_recursive_v1_files()
+                            #print(fileDict)
+                            # Delete Files
+                            for fileName, filePrinterPath in flatten(fileDict) :
+                                if fileName not in listFileToSend :
+                                    print("  * Delete File : " + filePrinterPath, end="")
+                                    ret = prusaMini.delete_gcode(filePrinterPath)
+                                    if ret.status_code == 204 :
+                                        print(" -> Done")
                                     else :
-                                        print(" -> Sending file", end="")
-                                        ret = prusaMini.put_gcode(folderGcode.path, folderGcode.name)
-                                        print(" - Status : " + str(ret.status_code) + " text "  + str(ret.text))
+                                        print(" -> Error : " + str(ret.status_code) + " text "  + str(ret.text))
+                            # Delete Folders
+                            for folderPrinterPath in flattenFolder(fileDict) :
+                                if folderPrinterPath not in listFolderToSend :
+                                    print("  * Delete Folder : /" + folderPrinterPath, end="")
+                                    ret = prusaMini.delete_gcode("/" + folderPrinterPath)
+                                    if ret.status_code == 204 :
+                                        print(" -> Done")
+                                    else :
+                                        print(" -> Error : " + str(ret.status_code) + " text "  + str(ret.text))
+                                
                 else :
                     print("  -> Warning : IP PKA not found : " + str(printerParam))
 
